542183a71d57eacdf14e06e2c380b9b4b309cd26
Robert Cicero <robert.cicero@iress.com>
2019-11-04 12:58:17 +0000

    Changed to 1 second because was getting billed for 3 seconds

--- aws/sqs.py
+++ aws/sqs.py
@@ -13,8 +13,8 @@ def getSQSMessages(client, queue_url):
     response = client.receive_message(
         QueueUrl=queue_url,
         MaxNumberOfMessages=10,
-        WaitTimeSeconds=5,
-        VisibilityTimeout=5
+        WaitTimeSeconds=1,
+        VisibilityTimeout=1
     )
 
     # SQS will return a object of type messages if there is anything on the queue
